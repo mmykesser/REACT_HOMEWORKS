bb76cc39d66c0de74cbd8a8fffe2fff59a902fc7
Maksym Polinovskyi <mmykesser@gmail.com>
2024-07-15 14:13:50 +0200

    new configuration v2

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
